7e74048efa963109e059de87e9c69f96e5e4d98f
Jean-Michel Bernard <bernard.jeanmichel@gmail.com>
2022-04-08 09:51:11 +0800

    Extract reusable String constants + Improve unit test coverage

--- src/main/java/org/omir/euler/problems10/problem10/SummationOfPrimes.java
+++ src/main/java/org/omir/euler/problems10/problem10/SummationOfPrimes.java
@@ -13,10 +13,22 @@ import org.omir.euler.utils.math.NumberUtils;
  */
 public class SummationOfPrimes {
 
-	private static final int TWO_MILLION = 2000000;
+	private SummationOfPrimes() {}
+	
+	public static final int TWO_MILLION = 2000000;
+	
+	public static final String SOLUTION_PREFIX = "The sum of all the primes below two million is ";
 
 	public static void main(String[] args) {
-		System.out.println("The sum of all the primes below two million is " + NumberUtils.sumOfPrimesBelow(TWO_MILLION));
+		System.out.println(displaySolution());
+	}
+
+	public static String displaySolution() {
+		return SOLUTION_PREFIX + sumOfPrimesBelowTwoMillion();
+	}
+
+	public static long sumOfPrimesBelowTwoMillion() {
+		return NumberUtils.sumOfPrimesBelow(TWO_MILLION);
 	}
 
 }
